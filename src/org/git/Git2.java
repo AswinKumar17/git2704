341ebcebf6562890b2ed13e496a92ae2c8b6afb2
AswinKumar17 <aswinkumar1710@gmail.com>
2022-04-27 12:02:21 +0530

    Message1

---
+++ src/org/git/Git2.java
@@ -0,0 +1,20 @@
+package org.git;
+public class Git2 {
+	public void collegeName() {
+		System.out.println("The name of the college is Greens");
+		}
+		public void collegeCode() {
+		System.out.println("The college code is 1122");
+		}
+		public void collegeRank() {
+		System.out.println("The rank of college is 12");
+		}
+		public static void main(String[] args) {
+			Git2 c = new Git2();
+			c.collegeName();
+			c.collegeCode();
+			c.collegeRank();
+		}
+
+
+}
